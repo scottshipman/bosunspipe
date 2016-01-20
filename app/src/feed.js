9f640d3ab50c8d9a2c73b562cff9d1817b3f7782
jordanvector <jvschin@gmail.com>
2016-01-19 23:40:02 -0500

    Got the first image grabbing script working

--- app/src/feed.js
+++ app/src/feed.js
@@ -8,15 +8,28 @@ App.controller("FeedCtrl", ['$scope','FeedService', function ($scope,Feed) {
         Feed.parseFeed($scope.feedSrc).then(function(res){
             $scope.loadButonText=angular.element(e.target).text();
             $scope.feeds=res.data.responseData.feed.entries;
-            var findFirstImage = $scope.feeds[0].content;
-            angular.forEach($scope.feeds,function(value){
+            angular.forEach($scope.feeds,function(value, key){
 
+              var content = value.content;
+
+              // var img = content.find('img').src;
+              var doesitwork = $(content).find('img').attr('src');
+              console.log(doesitwork);
+              // console.log(doesitwork)
               // var firstImage = $(value.content).find('img').eq(0).attr('src');
-              console.log(value.content);
+              if ('undefined' !== typeof doesitwork) {
+                var imageField = $scope.feeds[key];
+                imageField.img = doesitwork;
+
+                // $scope.feeds[key].push(doesitwork);
+                // value.push({img : doesitwork});
+                console.log(imageField);
+                // console.log(key);
+              }
             });
-          var firstImage = $(findFirstImage).find('img').eq(0).attr('src');
-          console.log(firstImage);
-          $scope.firstImg = firstImage;
+          // var firstImage = $(findFirstImage).find('img').eq(0).attr('src');
+          // console.log(firstImage);
+          // $scope.firstImg = firstImage;
         });
     }
 }]);
